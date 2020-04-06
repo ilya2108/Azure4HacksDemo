ed62d715e13896171f7f26195d372a2e0f2b2ef5
ryabuily <ryabuily@fit.cvut.cz>
2020-04-06 21:28:18 +0300

    Mobile App done

--- Mobile App/ExamSender/ExamSender/MainPage.xaml.cs
+++ Mobile App/ExamSender/ExamSender/MainPage.xaml.cs
@@ -5,17 +5,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace ExamSender
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+    /// <summary>
+    /// Main (and only) page of the application
+    /// </summary>
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        /// <summary>
+        /// Link to the Azure Logic App (to be added)
+        /// </summary>
+        private const string link = "<LINK_TO_LOGIC_APP>";
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handler of send button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void OnSendClicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var obj = new
+                    {
+                        name = Name.Text,
+                        surname = Surname.Text,
+                        work = Work.Text
+                    };
+
+                    var payload = JsonConvert.SerializeObject(obj);
+                    var content = new StringContent(payload, Encoding.UTF8, @"application/json");
+                    var response = await client.PostAsync(link, content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseBody = await response.Content.ReadAsStringAsync();
+                        if (responseBody == "true")
+                        {
+                            await DisplayAlert("Ура!", "Работа успешно отправлена.", "OK");
+                        }
+                    }
+                    else
+                        throw new InvalidOperationException();
+                }
+            }
+            catch
+            {
+                await DisplayAlert("Увы", "Что-то пошло не так. Попробуйте снова.", "OK");
+            }
+            
+            
         }
     }
 }
